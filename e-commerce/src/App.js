c930c0c1bc8a324969e50080f9f5fa1e31287422
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-28 14:41:12 +0100

    Added sass file

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import HomePage from "./components/homepage";
 
 function App() {
   return (
-    <div >
-      Hello world
+    <div>
+      <HomePage />
     </div>
   );
 }
