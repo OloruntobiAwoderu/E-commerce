2f74ba9e3faf428f20677b13c0caf219a11d0124
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-29 14:16:42 +0100

    updated app.js

--- e-commerce/src/components/directory/directory.jsx
+++ e-commerce/src/components/directory/directory.jsx
@@ -45,7 +45,7 @@ class Directory extends Component {
   render() {
     return <div className="directory-menu" >
         {this.state.sections.map(section => (
-            <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size}/>
+            <MenuItem key={section.id} title={section.title} imageUrl={section.imageUrl} size={section.size} linkUrl={section.linkUrl}/>
         ))}
     </div>;
   }
