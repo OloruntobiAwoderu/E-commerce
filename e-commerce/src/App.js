1541d9c246fe2e219ba8e1d393265acf048a1097
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-29 17:50:50 +0100

    imported sign in and sign up page to app.js

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -4,6 +4,7 @@ import HomePage from "./pages/homepage/homepage";
 import { Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop";
 import Header from "./components/header/header";
+import AuthenticationForm from "./pages/signIn-SignUp/signIn-SignUp";
 
 const HatsPage = () => (
   <div>
@@ -19,6 +20,7 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
         <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/signin" component={AuthenticationForm} />
       </Switch>
     </div>
   );
