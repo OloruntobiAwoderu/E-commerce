a88b08b74877c10e9d0e478b76ccedafd0979fb7
oloruntobi <awoderuoloruntobi@gmail.com>
2019-11-29 17:50:09 +0100

    completed signin component

---
+++ e-commerce/src/components/sign-in/signIn.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+  handleSubmit = e => {
+    e.preventDefault();
+    this.setState({ email: "", password: "" });
+  };
+  handleChange = e => {
+    const { value, name } = e.target;
+    this.setState({ [name]: value });
+  };
+  render() {
+    return (
+      <div>
+        <h2>I already have an account</h2>
+        <span>Sign in with your email and password</span>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+            onChange={this.handleChange}
+          />
+          <label>Email</label>
+          <input
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input type="submit" value="submit form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
