388f8d1c96260ad16649e5fa18dfffa4294320d1
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-01 18:08:01 +0100

    added sign out to header component

--- e-commerce/src/components/header/header.jsx
+++ e-commerce/src/components/header/header.jsx
@@ -2,8 +2,10 @@ import React, { Component } from "react";
 import "./header.scss";
 import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => {
+
+const Header = ({ currentUser }) => {
   return (
     <div className="header">
       <Link to="/" className="logo-container">
@@ -16,6 +18,7 @@ const Header = () => {
         <Link to="/shop" className="option">
           CONTACT
         </Link>
+        { currentUser ? <div className="option" onClick={() => auth.signOut()}>SIGN OUT</div> : <Link className="option" to="/signin" >SIGN IN</Link>}
       </div>
     </div>
   );
