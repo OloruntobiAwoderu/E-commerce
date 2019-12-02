221332d98ec06dc990e36a9817da3b910041d8d4
oloruntobi <awoderuoloruntobi@gmail.com>
2019-12-02 18:21:30 +0100

    Implemented firebase signup with emil and password

--- e-commerce/src/pages/signIn-SignUp/signIn-SignUp.jsx
+++ e-commerce/src/pages/signIn-SignUp/signIn-SignUp.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './signIn.scss';
 import SignIn from '../../components/sign-in/signIn';
+import Signup from '../../components/sign-up/signup';
 
 const AuthenticationForm = () => {
     return (<div className="Authentication">
          <SignIn/>
+         <Signup />
     </div>  );
 }
  
