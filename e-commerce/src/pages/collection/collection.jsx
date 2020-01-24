38449931c43842a9f85b5a24bcf90cb847ae0185
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-24 14:51:43 +0100

    Chore: routing works

--- e-commerce/src/pages/collection/collection.jsx
+++ e-commerce/src/pages/collection/collection.jsx
@@ -4,16 +4,25 @@ import { selectCollection } from "../../redux/shop/shop.selector"
 import CollectionItem from "../../components/collection-item/collection-item";
 import "./collection.scss";
 
-const Collection = ({ collection }) => {
+
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
   return (
-    <div className="category">
-      <h2>CATEGORY PAGE</h2>
+    <div className='collection-page'>
+      <h2 className='title'>{title}</h2>
+      <div className='items'>
+        {items.map(item => (
+          <CollectionItem key={item.id} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
 
 const mapStateToProps = (state, ownProps) => ({
   collection: selectCollection(ownProps.match.params.collectionId)(state)
-})
+});
 
-export default connect(mapStateToProps)(Collection);
+
+
+export default connect(mapStateToProps)(CollectionPage);
--- e-commerce/src/pages/shop/shop.jsx
+++ e-commerce/src/pages/shop/shop.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 import { Route } from 'react-router-dom'
-import collectionsOverview from "../../components/collections-overview/collections-overview";
-import Collection from '../collection/collection'
+import CollectionsOverview from "../../components/collections-overview/collections-overview";
+import CollectionPage from '../collection/collection'
 
-const ShopPage = ({ match }) => {
-  return (
-    <div className="shop-page">
-      <Route exact path={`${match.path}`} component={collectionsOverview} />
-      <Route path={`${match.path}/:categoryId`} component={Collection} />
-    
-    </div>
-  );
-};
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
+
 
 export default ShopPage;
