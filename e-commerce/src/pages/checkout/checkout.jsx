e794bd8bcb1cfd719ba86f15d1c3ec016c724505
oloruntobi <awoderuoloruntobi@gmail.com>
2020-01-25 21:20:25 +0100

    Made additional changes

--- e-commerce/src/pages/checkout/checkout.jsx
+++ e-commerce/src/pages/checkout/checkout.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart-selector'
 import CheckoutItem from '../../components/checkout-item/checkout-item'
-
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button'
 
 const checkoutPage = ({ cartItems, total}) => {
   return (
@@ -30,8 +30,14 @@ const checkoutPage = ({ cartItems, total}) => {
           cartItems.map(cartItem => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
       }
       <div className="total">
-    <span>TOTAL: ${total}</span>
+    TOTAL: ${total}
       </div>
+      <div className="test-warning">
+        *Please use the following test credit card for payments*
+        <br/>
+        4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </div>
   );
 };
